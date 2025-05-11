8f36a30fde1fedff410a828064daffb070ff783b
Yusuf Msabeh <yussef.msabeh@gmail.com>
2025-05-11 12:11:59 +0300

    feat

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import {mainController} from "./controllers/main.js";
+import ServerlessHttp from "serverless-http";
 const app = express()
 const port = 3000
 app.use(express.json())
 app.get('/', mainController)
-app.listen(port, "localhost", () => console.log(`Listening on port ${port}`))
+const handler2 = ServerlessHttp(app)
+export const handler = async (event,context)=>{
+    const result = await handler2(event, context);
+    return result;
+}
